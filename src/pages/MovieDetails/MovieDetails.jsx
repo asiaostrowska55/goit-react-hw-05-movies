2b9fb22c6dcff4d470070376d9c0e444c56849c5
asiaostrowska55 <110023821+asiaostrowska55@users.noreply.github.com>
2023-04-30 13:26:56 +0200

    movie details

--- src/pages/MovieDetails/MovieDetails.jsx
+++ src/pages/MovieDetails/MovieDetails.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState, Suspense } from 'react';
 import css from './MovieDetails.module.css';
 import { useParams, useLocation } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import { Link } from '../../App.styled';
 import { fetchMovieDetails } from 'functions/api';
 
@@ -22,12 +23,69 @@ const MovieDetails = () => {
     };
     fetchDetails();
   }, [id]);
+
+  let genres = [...movieDetails.genres].map(genre => genre.name).join(', ');
+
   return (
-    <div>
-      <Link to={backLinkHref} className={css.movieLink}>
-        &lt; Go back
-      </Link>
-    </div>
+    <main>
+      <div>
+        <Link to={backLinkHref} className={css.movieLink}>
+          &lt; Go back
+        </Link>
+        <div>
+          <img src={movieDetails.poster_path} alt={movieDetails.title}></img>
+          <div className={css.movieDesc}>
+            <h1 className={css.movieDescTitle}>{movieDetails.title}</h1>
+            <span className={css.movieDescInfo}>
+              User Score: {movieDetails.voteAverage}{' '}
+            </span>
+            <h2 className={css.movieDescCategory}>Overview</h2>
+            <span className={css.movieDescInfo}>{movieDetails.overview}</span>
+            <h2 className={css.movieDescCategory}>Genres</h2>
+            <span className={css.movieDescInfo}>{genres}</span>
+          </div>
+        </div>
+        <section>
+          <span>Additional information:</span>
+          <ul className={css.movieOthersList}>
+            <li className={css.movieOthersListItem}>
+              <Link
+                to="cast"
+                className={css.movieOthersListLink}
+                state={{ from: location.state.from }}
+              >
+                Cast
+              </Link>
+            </li>
+            <li className={css.movieOthersListItem}>
+              <Link
+                to="reviews"
+                className={css.movieOthersListLink}
+                state={{ from: location.state.from }}
+              >
+                Reviews
+              </Link>
+            </li>
+          </ul>
+        </section>
+        <section>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Outlet />
+          </Suspense>
+        </section>
+      </div>
+    </main>
   );
 };
 export default MovieDetails;
+
+{
+  /* <ul className={[css.movie__genres]}>
+            {genres.map((genre, index) => (
+              <li key={genre.id}>
+                {genre.name}
+                {index !== genres.length - 1 ? ',' : ''}
+              </li>
+            ))}
+          </ul> */
+}
--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,16 +1,12 @@
 import css from './NotFound.module.css';
 
-const NotFound = () => {
+const NotFound = homepage => {
   return (
     <div>
       <div>
         <h1>404</h1>
         <h2>UH OH! You're lost.</h2>
         <p>The page you are looking for does not exist.</p>
-        <a href="#">
-          {' '}
-          <button className={css.btn}>HOME</button>
-        </a>
       </div>
     </div>
   );
