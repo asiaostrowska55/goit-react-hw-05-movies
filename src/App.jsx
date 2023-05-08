7a5ffd23dda78e61ba2061c9d12eec30894a1744
asiaostrowska55 <110023821+asiaostrowska55@users.noreply.github.com>
2023-05-09 00:05:01 +0200

    cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,6 @@ export const App = () => {
       </Suspense>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
-          {/* <Route path="/" element={<Header />}> */}
           <Route path="/" element={<Home />} />
           <Route path="/movies" element={<Movies />} />
           <Route path="/movies/:movieId" element={<MovieDetails />}>
